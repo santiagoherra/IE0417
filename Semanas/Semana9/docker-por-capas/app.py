7185ec46b126bc7c9a815ae333b16a24b091b052
Santiago Herra <tato.herra@gmail.com>
2025-10-07 23:17:08 -0600

    agregando imagenes del progreso del laboratorio y codigo de archivo Dockerfile.dev

--- Semanas/Semana9/docker-por-capas/app.py
+++ Semanas/Semana9/docker-por-capas/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    saludo = os.getenv("SALUDO", "Hola desde la imagen")
+    saludo = os.getenv("SALUDO", "Hola, clase IE0417")
     return jsonify({"ok": True, "mensaje": saludo})
 
 if __name__ == "__main__":
